f309b16258f1c84573daeed127740fc5af2c1b2c
bastonc <bastonsv@gmail.com>
2022-10-23 16:45:58 +0300

    HW28 added settings for github

--- src/config/settings/dev.py
+++ src/config/settings/dev.py
@@ -1,35 +1,24 @@
 from config.settings.base import *  # noqa
 
 CURRENT_ENV = "DEV"
+print(CURRENT_ENV)
 
 DEBUG = True
 
 ALLOWED_HOSTS = []
 
-if os.environ.get("GITHUB_WORKFLOW"):
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": "github_actions",
-            "USER": "postgres",
-            "PASSWORD": "postgres",
-            "HOST": "127.0.0.1",
-            "PORT": "5432",
-        },
-    }
-else:
-    DATABASES = {
-        "default_sqlite": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        },
-        "default": {
-            "ENGINE": "django.db.backends.postgresql",
-            "NAME": os.environ.get("POSTGRES_DB"),
-            "USER": os.environ.get("POSTGRES_USER"),
-            "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
-            "HOST": os.environ.get("POSTGRES_HOST"),
-            "PORT": os.environ.get("POSTGRES_PORT"),
-            "TEST": {"NAME": "test"},
-        },
-    }
+DATABASES = {
+    "default_sqlite": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
+    },
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB"),
+        "USER": os.environ.get("POSTGRES_USER"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD"),
+        "HOST": os.environ.get("POSTGRES_HOST"),
+        "PORT": os.environ.get("POSTGRES_PORT"),
+        "TEST": {"NAME": "test"},
+    },
+}
