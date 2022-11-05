863799e0b35c2ab0d2a70379394b5497ed450d51
bastonc <bastonsv@gmail.com>
2022-11-05 12:17:09 +0200

    HW31-celery redis admin

--- src/core/tasks.py
+++ src/core/tasks.py
@@ -10,6 +10,7 @@ from shop.models import Category, SubCategory, Product, Brand
 @shared_task
 def generate_category():
     categories_name = ['Mixing consoles', 'Microphones', 'ADC-DAC', 'Processors', 'Compressors', "Equalizers"]
+    category_obj_list = []
     for category in categories_name:
         param = {
             "index": True,
@@ -19,7 +20,8 @@ def generate_category():
             "alias": str(category).lower().replace(' ', '-'),
             "image": settings.CATEGORY_IMAGE_DEFAULT
         }
-        category = Category.objects.create(**param)
+        category_obj_list.append(Category(**param))
+    Category.objects.bulk_create(category_obj_list)
 
 
 @shared_task
@@ -30,6 +32,7 @@ def generate_sub_category():
                       "processors": ["Reverb", "Pitch-tone", "Multiprocessing", "Delay"],
                       "compressors": ["Multiband", "Analog compressor", "Digital compressor"],
                       "equalizers": ["Parametric", "Graphic", "Paragraphic"]}
+    sub_category_obj_list = []
     for category, sub_categories_list in sub_categories.items():
         current_category = Category.objects.filter(alias=category).get()
         for sub_category in sub_categories_list:
@@ -42,13 +45,15 @@ def generate_sub_category():
                 "category": current_category,
                 "image": settings.SUB_CATEGORY_IMAGE_DEFAULT
             }
-            sub_category = SubCategory.objects.create(**param)
+            sub_category_obj_list.append(SubCategory(**param))
+    SubCategory.objects.bulk_create(sub_category_obj_list)
 
 
 @shared_task
 def generate_product():
     fake = Faker(locale="en")
     brands_list = ["Allen&Heath", "Soundcraft", "Drawmer", "Klark-Tekhnik", "RME"]
+    brand_obj_list = []
     for brand in brands_list:
         param = {
             "index": True,
@@ -57,9 +62,11 @@ def generate_product():
             "seo_title": f"{brand} in Baston Sound Shop",
             "seo_description": f"{brand} in Baston Sound Shop",
         }
-        Brand.objects.create(**param)
+        brand_obj_list.append(Brand(**param))
+    Brand.objects.bulk_create(brand_obj_list)
     sub_categories = SubCategory.objects.all()
     brands = Brand.objects.all()
+    product_obj_list = []
     for sub_category in sub_categories:
         for _ in range(5):
             brand = brands[random.randint(0, 4)]
@@ -76,4 +83,5 @@ def generate_product():
                 "price": 10000,
                 "currency": "UAH"
             }
-            Product.objects.create(**param)
+            product_obj_list.append(Product(**param))
+    Product.objects.bulk_create(product_obj_list)
