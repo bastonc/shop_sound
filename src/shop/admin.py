6716c726d3b2a175abf6f2967b5bfd587830bbc0
bastonc <bastonsv@gmail.com>
2022-11-20 23:29:24 +0200

    Added listing in admin order and product

--- src/shop/admin.py
+++ src/shop/admin.py
@@ -6,20 +6,30 @@ from shop.models import (Brand, Category, Order, OrderItem, Pages, Product,
 
 class OrderItemInline(admin.TabularInline):
     model = OrderItem
-    raw_id_fields = ["product"]
+    readonly_fields = ("user", "product", "price", "quantity")
+    extra = 0
 
 
-# @admin.register(Order)
-# class OrderAdmin(admin.ModelAdmin):
-#     list_display = ['id', 'first_name', 'last_name', 'email',
-#                     'address', 'postal_code', 'city', 'paid',
-#                     'created', 'updated']
-#     list_filter = ['paid', 'created', 'updated']
-#     #inlines = [OrderItemInline]
-#     # class Meta:
-#     #     ordering = ('-created',)
-#     #     verbose_name = 'OrderAdmin'
-#     #     verbose_name_plural = 'OrdersAdmin'
-#
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ('id', 'first_name', 'last_name', 'email',
+                    'address', 'postal_code', 'city', 'paid',
+                    'created', 'updated', 'get_total_cost')
+    list_filter = ['paid', 'created', 'updated']
+    inlines = [OrderItemInline]
+    class Meta:
+        ordering = ('-created',)
+        verbose_name = 'Order'
+        verbose_name_plural = 'Orders'
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['name', 'brand', 'price', 'url', 'id']
+    class Meta:
+        ordering = ('-created',)
+        verbose_name = 'Product'
+        verbose_name_plural = 'Products'
 # # Register your tests here.
-admin.site.register([Order, OrderItem, Category, SubCategory, Brand, Product, Pages])
+admin.site.register([OrderItem, Category, SubCategory, Brand, Pages])
